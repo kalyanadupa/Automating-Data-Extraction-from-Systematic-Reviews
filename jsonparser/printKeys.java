bef1b4b7bb73f48288af6f942ed2bafd6c72d400
Abhishek <Abhishek@abhi>
2015-10-01 18:05:51 +0530

    Backup

--- jsonparser/printKeys.java
+++ jsonparser/printKeys.java
@@ -9,7 +9,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,85 +16,85 @@ import java.util.List;
  *
  * @author Abhishek
  */
-public class groupKeys {
-    public static void main(String[] argsv) throws FileNotFoundException, IOException{
+public class printKeys {
+    public static void main(String[] argsv) throws FileNotFoundException, IOException {
         List<Key> temp = new ArrayList<Key>();
-        
+
         BufferedReader br = new BufferedReader(new FileReader(new File("topFreqKeys")));
         String line = "";
-        while ((line = br.readLine()) != null){
+        while ((line = br.readLine()) != null) {
             String[] token = line.split("\t");
-            Key k = new Key(token[0],Integer.parseInt(token[1]),token[2],countWords(token[0]));
+            Key k = new Key(token[0], Integer.parseInt(token[1]), token[2], countWords(token[0]));
             temp.add(k);
         }
         List<Key> removeList = new ArrayList<Key>();
-        for(Key k : temp){
-            if(k.name.startsWith(" ")){
+        for (Key k : temp) {
+            if (k.name.startsWith(" ")) {
                 k.name = k.name.trim();
-                for(Key p : temp){
-                    if(k.name.equalsIgnoreCase(p.name) && k.category.contains(p.category) && (k.freq != p.freq)){
-                        if(k.freq > p.freq){
+                for (Key p : temp) {
+                    if (k.name.equalsIgnoreCase(p.name) && k.category.contains(p.category) && (k.freq != p.freq)) {
+                        if (k.freq > p.freq) {
                             k.gKey.add(p);
-                            k.freq = k.freq +p.freq;
+                            k.freq = k.freq + p.freq;
                             removeList.add(p);
-                            System.out.println("Grouped " + k.name +"("+k.freq+")" +" <- " + p.name+"("+p.freq+")");
+                            System.out.println("Grouped " + k.name + "(" + k.freq + ")" + " <- " + p.name + "(" + p.freq + ")");
                         }
-                        if(k.freq < p.freq){
+                        if (k.freq < p.freq) {
                             p.gKey.add(k);
-                            p.freq = k.freq +p.freq;
+                            p.freq = k.freq + p.freq;
                             removeList.add(k);
-                            System.out.println("Grouped " + p.name+"("+p.freq+")" +" <- " + k.name+"("+k.freq+")");
-                        }   
+                            System.out.println("Grouped " + p.name + "(" + p.freq + ")" + " <- " + k.name + "(" + k.freq + ")");
+                        }
                     }
                 }
             }
         }
-        for(Key x : removeList){
-            if(x.gKey.isEmpty())
+        for (Key x : removeList) {
+            if (x.gKey.isEmpty()) {
                 temp.remove(x);
-            else
-                System.out.println("Error"+x.name);
+            } else {
+                System.out.println("Error" + x.name);
+            }
         }
-        
+
         CosineSimilarity cs = new CosineSimilarity();
-        for(int i = 0; i < temp.size();i++){
-            for(int j = i+1; j < temp.size();j++){
-                double sim = cs.CosineSimilarity_Score(temp.get(i).name,temp.get(j).name);
-                int ed = minDistance(temp.get(i).name,temp.get(j).name);
-                if(ed == 1){
+        for (int i = 0; i < temp.size(); i++) {
+            for (int j = i + 1; j < temp.size(); j++) {
+                double sim = cs.CosineSimilarity_Score(temp.get(i).name, temp.get(j).name);
+                int ed = minDistance(temp.get(i).name, temp.get(j).name);
+                if (ed == 1) {
                     if ((temp.get(i).freq > temp.get(j).freq) && (temp.get(i).category.contains(temp.get(j).category))) {
                         temp.get(i).gKey.add(temp.get(j));
-                        System.out.println("Grouped " + temp.get(i).name+"("+temp.get(i).freq+")" +" <- " + temp.get(j).name+"("+temp.get(j).freq+")");
+                        System.out.println("Grouped " + temp.get(i).name + "(" + temp.get(i).freq + ")" + " <- " + temp.get(j).name + "(" + temp.get(j).freq + ")");
                     } else if ((temp.get(i).freq < temp.get(j).freq) && (temp.get(j).category.contains(temp.get(i).category))) {
                         temp.get(j).gKey.add(temp.get(i));
-                        System.out.println("Grouped " + temp.get(j).name+"("+temp.get(j).freq+")" +" <- " + temp.get(i).name+"("+temp.get(i).freq+")");
+                        System.out.println("Grouped " + temp.get(j).name + "(" + temp.get(j).freq + ")" + " <- " + temp.get(i).name + "(" + temp.get(i).freq + ")");
                     }
-                }
-                else if(sim > 0.7){
-                    if((temp.get(i).freq > temp.get(j).freq) && (temp.get(i).category.contains(temp.get(j).category))){
+                } else if (sim > 0.7) {
+                    if ((temp.get(i).freq > temp.get(j).freq) && (temp.get(i).category.contains(temp.get(j).category))) {
                         temp.get(i).gKey.add(temp.get(j));
-                        System.out.println("Grouped " + temp.get(i).name+"("+temp.get(i).freq+")" +" <- " + temp.get(j).name+"("+temp.get(j).freq+")");
-                    } 
-                    else if((temp.get(i).freq < temp.get(j).freq) && (temp.get(j).category.contains(temp.get(i).category))){
+                        System.out.println("Grouped " + temp.get(i).name + "(" + temp.get(i).freq + ")" + " <- " + temp.get(j).name + "(" + temp.get(j).freq + ")");
+                    } else if ((temp.get(i).freq < temp.get(j).freq) && (temp.get(j).category.contains(temp.get(i).category))) {
                         temp.get(j).gKey.add(temp.get(i));
-                        System.out.println("Grouped " + temp.get(j).name+"("+temp.get(j).freq+")" +" <- " + temp.get(i).name+"("+temp.get(i).freq+")");
+                        System.out.println("Grouped " + temp.get(j).name + "(" + temp.get(j).freq + ")" + " <- " + temp.get(i).name + "(" + temp.get(i).freq + ")");
                     }
                 }
-                
+
             }
         }
-        
-        for(Key k : temp){
-            if(!k.gKey.isEmpty()){
+
+        for (Key k : temp) {
+            if (!k.gKey.isEmpty()) {
                 System.out.print(k.name + " <- ");
-                for(Key p : k.gKey)
-                    System.out.print( p.name +", ");
+                for (Key p : k.gKey) {
+                    System.out.print(p.name + ", ");
+                }
                 System.out.println("");
             }
-            
+
         }
     }
-    
+
     public static int countWords(String s) {
 
         int wordCount = 0;
@@ -107,12 +106,12 @@ public class groupKeys {
             // if the char is a letter, word = true.
             if (Character.isLetter(s.charAt(i)) && i != endOfLine) {
                 word = true;
-            // if char isn't a letter and there have been letters before,
+                // if char isn't a letter and there have been letters before,
                 // counter goes up.
             } else if (!Character.isLetter(s.charAt(i)) && word) {
                 wordCount++;
                 word = false;
-            // last word of String; if it doesn't end with a non letter, it
+                // last word of String; if it doesn't end with a non letter, it
                 // wouldn't count without this.
             } else if (Character.isLetter(s.charAt(i)) && i == endOfLine) {
                 wordCount++;
@@ -120,7 +119,6 @@ public class groupKeys {
         }
         return wordCount;
     }
-
 
     public static int minDistance(String word1, String word2) {
         int len1 = word1.length();
@@ -161,4 +159,5 @@ public class groupKeys {
 
         return dp[len1][len2];
     }
+    
 }
